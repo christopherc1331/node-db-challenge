ed5a36d9fa62c3055801b4d1a4d6717f76952e56
Chris Cruz <christopherc1331@gmail.com>
2019-11-22 19:30:58 -0800

    Added second table

--- data/migrations/20191122185531_createTables.js
+++ data/migrations/20191122185531_createTables.js
@@ -1,14 +1,38 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("projects", tbl => {
-    tbl.increments();
-    tbl
-      .string("name", 128)
-      .unique()
-      .notNullable();
-    tbl.string("completed", 128);
-    tbl.boolean("completed").defaultTo(flase);
-  });
-  // .createTable("");
+  return knex.schema
+
+    .createTable("projects", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("name", 128)
+        .unique()
+        .notNullable();
+
+      tbl.string("description", 128);
+
+      tbl.boolean("completed").defaultTo(flase);
+    })
+
+    .createTable("tasks", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("name")
+        .unique()
+        .notNullable();
+
+      tbl.string("description", 128);
+
+      tbl.boolean("completed").defaultTo(flase);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = function(knex) {};
